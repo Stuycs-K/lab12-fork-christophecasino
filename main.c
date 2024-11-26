0e0e34937e50929c0adc2659224934a49b114a4c
Christophe Casino <ccasino50@stuy.edu>
2024-11-26 18:52:23 -0500

    added rand int helper function and forking to main

--- main.c
+++ main.c
@@ -7,7 +7,31 @@
 #include <sys/stat.h>
 #include <errno.h>
 
+int rand_int(){
+  FILE *fd = open("/dev/random", O_RDONLY, 0);
+  if(fd == -1){
+    printf("%s\n", strerror(errno));
+    exit(1);
+  }
+  uint arr[1];
+  read(fd, arr, 4);
+  arr[0] = arr[0] % 5 + 1;
+  close(fd);
+  return arr[0];
+}
+
 int main(){
-  
+  int pid_var = getpid();
+  int num = rand_int();
+  pid_t p;
+  p = fork();
+  if(p<0){
+    perror("fork fail");
+    exit(1);
+  } else if ( p == 0){
+      printf("pid: %d rand_num: %d\n", pid_var, num);
+  }else{
+      
+  }
   return 0;
 }
